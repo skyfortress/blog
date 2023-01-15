83a39ca4edab72dc6bf6e2763d46d3041e1fb77a
Alexey Bondarenko <sky4tress@gmail.com>
2023-01-15 14:16:32 +0000

    Updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,19 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addHandlebarsShortcode("active", function(current, needed) {
-        if(needed == '/') {
-            return current === needed ? 'active' : '';
-        }
-        return current.includes(needed) ? 'active' : '';
+    eleventyConfig.addHandlebarsShortcode("dayFormat", function(date) {
+        return new Date(date).getDate();
     });
+
+    eleventyConfig.addHandlebarsShortcode("monthFormat", function(date) {
+        return new Date(date).toLocaleString('us', { month: 'short' });
+    });
+
+    eleventyConfig.addHandlebarsShortcode("yearFormat", function(date) {
+        return new Date(date).getFullYear();
+    });
+
+    eleventyConfig.addCollection("sortedPosts", function(collectionApi) {
+        return collectionApi.getFilteredByTag('post').reverse();
+    });
+
     eleventyConfig.addPassthroughCopy("assets");
 };
---
+++ posts/new.md
@@ -0,0 +1,9 @@
+---
+title: One more post
+description: This is a post on My Blog about agile frameworks.
+date: Last Modified
+tags: post
+layout: blog-post
+---
+
+Test
